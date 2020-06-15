3a6767d9f710d18d64415a8db2bf7d8165eb12ae
Shivam Gupta <45766261+shivamgupta1999@users.noreply.github.com>
2020-06-15 18:45:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchMovies from './searchMovies';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="brand">Flix & Chill</h1>
+      <SearchMovies />
+      <p>Made by <a href="https://shivamgupta1999.github.io/MyCV/">Shivam Gupta</a></p>
+    </div>
+  );
+}
+
+export default App;
