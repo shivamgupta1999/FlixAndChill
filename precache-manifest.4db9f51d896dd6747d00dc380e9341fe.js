393c3c998c7ff73f6b0a4df7fd4465bba88a833d
ssb <ssb.deciphered@gmail.com>
2020-06-15 18:38:48 +0530

    Updates

---
+++ precache-manifest.4db9f51d896dd6747d00dc380e9341fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66284a9221fd6cc3bcd270e6fd8e8a68",
+    "url": "/FlixAndChill/index.html"
+  },
+  {
+    "revision": "552ac2f38286f7faf71b",
+    "url": "/FlixAndChill/static/js/2.21f9648a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FlixAndChill/static/js/2.21f9648a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6647c9a0c71c31d4e971",
+    "url": "/FlixAndChill/static/js/main.bbd4b128.chunk.js"
+  },
+  {
+    "revision": "d915232157b33769348d",
+    "url": "/FlixAndChill/static/js/runtime-main.74acecae.js"
+  }
+]);
